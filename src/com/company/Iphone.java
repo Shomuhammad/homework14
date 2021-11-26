469999b52dda2f22fb9d4c5b81db7d8d0210b7d7
Asus <Shomuhammad>
2021-11-26 19:33:32 +0600

    homework14

---
+++ src/com/company/Iphone.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Iphone {
+    private String name;
+     private int price;
+     private int camera;
+     boolean wifi;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCamera() {
+        return camera;
+    }
+
+    public void setCamera(int camera) {
+        this.camera = camera;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[]args) {
-            String name = "Iphone";
-            int price = 15000;
-            int cameraMP = 200;
-            boolean wifi = true;
+Iphone iphone = new Iphone();
+iphone.setName("Iphone");
+iphone.setPrice(15000);
+        iphone.setCamera(24);
+        iphone.setWifi(true);
+        System.out.println(iphone.getCamera()+ iphone.getPrice() + iphone.getName() + iphone.isWifi());
     }
 }
