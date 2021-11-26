f26bcdf92484e1a46a0eeb942a1982b78d85534f
Asus <Shomuhammad>
2021-11-26 16:53:55 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[]args) {
+            String name = "Iphone";
+            int price = 15000;
+            int cameraMP = 200;
+            boolean wifi = true;
+    }
+}
